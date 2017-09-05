f89789ab56f823d25ae3f9898e95d2f5ee5b852e
vladvasylets <vladvasylets@yanpix.com>
2017-09-05 17:31:37 +0300

    Add project files.

---
+++ TaskWebApp/ApiControllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+using System.Web.Http;
+
+namespace TaskWebApp.ApiControllers
+{
+    [Authorize]
+    public class AccountController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult SignUpSignIn()
+        {
+            //HttpContext.GetOwinContext().Authentication.Challenge();
+            return Ok();
+        }
+    }
+}
---
+++ TaskWebApp/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web;
+using System.Web.Http;
+
+namespace TaskWebApp.App_Start
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration configuration)
+        {
+            // Web API configuration and services
+
+            configuration.Routes.MapHttpRoute("API Default", "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+
+            // Web API routes
+            //configuration.MapHttpAttributeRoutes();
+
+            //var jsonFormatter = configuration.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            //jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
